848160653a75db404025f3c0095e3e340736fc78
Ryan Tay <rt385@cornell.edu>
2023-01-11 15:42:38 -0500

    possible arguments to generate signed url

--- EndpointContainer/routes.py
+++ EndpointContainer/routes.py
@@ -62,6 +62,8 @@ def upload (request):
         method="PUT",
         # Use updated credentials (new token for signature is created by Google every 12 hours)
         credentials=credentials
+        # service_account_email=credentials.service_account_email,
+        # access_token=credentials.token,
     )
 
     # print("Generated PUT signed URL:")

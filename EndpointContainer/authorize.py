690dba72d36422ffe1bf8260e49ca0d2f86e03d2
Ryan Tay <rt385@cornell.edu>
2023-01-17 12:00:12 -0500

    import typo

--- EndpointContainer/authorize.py
+++ EndpointContainer/authorize.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from arix_auth.legacy.util import is_configured, current_session
+from arxiv_auth.legacy.util import is_configured, current_session
 
 import config
 from Submissions import Submissions

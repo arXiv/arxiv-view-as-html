3e5e3175c8281f94b982c95b86fc6c01e29e57d4
Mark Nazzaro <marknazzaro2@gmail.com>
2023-10-25 17:02:32 -0400

    Same fix in another place

--- ConversionContainer/source/publish/db_queries.py
+++ ConversionContainer/source/publish/db_queries.py
@@ -40,7 +40,7 @@ def write_published_html (paper_id: str, version: int, html_submission: DBLaTeXM
 def get_submission_timestamp (submission_id: int) -> Optional[str]:
     try:
         query = text("SELECT submit_time from arXiv_submissions WHERE submission_id=:submission_id")
-        query.bindparams(submission_id=submission_id)
+        query = query.bindparams(submission_id=submission_id)
         ts: datetime = db.session.execute(query).scalar() # TODO: Add error handling
         return ts.strftime('%d %b %Y')
     except Exception as e:
@@ -50,7 +50,7 @@ def get_submission_timestamp (submission_id: int) -> Optional[str]:
 def get_version_primary_category (paper_id: str, version: int) -> Optional[str]:
     try:
         query = text("SELECT abs_categories FROM arXiv_metadata WHERE paper_id=:paper_id AND version=:version")
-        query.bindparams(paper_id=paper_id, version=version)
+        query = query.bindparams(paper_id=paper_id, version=version)
         return db.session.execute(query).scalar().split(' ')[0] # TODO: Needs to be tested
     except Exception as e:
         raise DBConnectionError from e

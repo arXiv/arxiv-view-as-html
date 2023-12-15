5aa204b6b8c54c2b8c919f100007312bbcae8667
Mark Nazzaro <marknazzaro2@gmail.com>
2023-12-15 13:20:40 -0500

    Remove extra stuff, and remove static step from CI/CD

--- HTMLPathReverseProxy/entry_point.py
+++
@@ -1,6 +0,0 @@
-from source.factory import create_web_app
-
-app = create_web_app()
-
-if __name__=='__main__':
-    app.run(debug=False)
--- storage_trigger.py
+++
@@ -1,8 +0,0 @@
-import functions_framework
-import requests
-
-CLOUD_RUN_URL = "https://latexml-conversion-gp2ubwi5mq-uc.a.run.app"
-
-@functions_framework.cloud_event
-def on_tex_source_uploaded (cloud_event):
-    requests.post(CLOUD_RUN_URL, cloud_event.data)

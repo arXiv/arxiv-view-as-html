9a5cee46dc83aefdadb907b79f91a147c9d10a85
Mark Nazzaro <marknazzaro2@gmail.com>
2024-02-12 13:35:26 -0500

    Try something else... Still getting two logs

--- ConversionContainer/entry_point.py
+++ ConversionContainer/entry_point.py
@@ -1,17 +1,17 @@
 """Initializes the Flask app"""
 import logging
 
-from google.cloud import logging as gcp_logging
+# from google.cloud import logging as gcp_logging
 
 from source.factory import create_web_app
 from source.models.db import db
 
 
-logging.basicConfig(level=logging.ERROR, 
+logging.basicConfig(level=logging.DEBUG, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-logging_client = gcp_logging.Client()
-logging_client.setup_logging()
+# logging_client = gcp_logging.Client()
+# logging_client.setup_logging()
 
 app = create_web_app()
 

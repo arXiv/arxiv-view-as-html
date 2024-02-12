ff7935a7d8dc99d8927c0e8a9369acb9da5f0389
Mark Nazzaro <marknazzaro2@gmail.com>
2024-02-12 15:20:39 -0500

    This will work

--- ConversionContainer/entry_point.py
+++ ConversionContainer/entry_point.py
@@ -1,16 +1,12 @@
 """Initializes the Flask app"""
 import logging
 
-# from google.cloud import logging as gcp_logging
+from google.cloud import logging as gcp_logging
 
 from source.factory import create_web_app
 from source.models.db import db
 
-
-logging.getLogger.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-
-logging_client = gcp_logging.Client()
-logging_client.setup_logging()
+gcp_logging.Client().setup_logging()
 
 app = create_web_app()
 

279546f628bf6917b979d04ef819c526156f16b7
Mark Nazzaro <marknazzaro2@gmail.com>
2024-02-19 20:43:36 -0500

    Added fastly purge

---
+++ ConversionContainer/source/publish/fastly_purge.py
@@ -0,0 +1,31 @@
+import logging
+
+from flask import current_app
+
+def fastly_purge_abs (paper_id: str, version: int, fastly_key: str):
+    headers = {
+        "Fastly-Key": fastly_key,
+        "Accept": "application/json",
+    }
+    domains = ["arxiv.org", "web3.arxiv.org", "www.arxiv.org"]
+    
+    for domain in domains:
+        url = f"https://{ domain }/abs/{ paper_id }"
+
+        response = requests.request("PURGE", url, headers=headers)
+
+        if response.status_code == 200:
+            success.append(url)
+        else:
+            status_code = 500
+            msg = 'Error purging'
+            failed.append(url)
+
+        url = f"https://{ domain }/abs/{ paper_id }v{ version }"
+
+        response = requests.request("PURGE", url, headers=headers)
+
+        if response.status_code == 200:
+            logging.info(f'successfully purged { url }')
+        else:
+            logging.warning(f'failed to purge { url }')
